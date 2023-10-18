bd7ed0b8676b90b9bc9b7f79eac84018eb98f0d2
Shuvojit Sana <shuvojitsanabd2004@gmai.com>
2023-10-19 02:11:02 +0600

    added nav section

---
+++ src/Sheard/Header/Header.jsx
@@ -0,0 +1,19 @@
+import "./Header.css";
+import logo from "../../images/Logo.svg";
+import { Link } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <nav className="header">
+      <img src={logo} alt="" />
+      <div >
+        <Link to="/shop">Shop</Link>
+        <Link to="/order">Order</Link>
+        <Link to="/inventory">Inventory</Link>
+        <Link to="/login">Login</Link>
+      </div>
+    </nav>
+  );
+};
+
+export default Header;
